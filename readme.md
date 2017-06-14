bccac6dedb437f30db029eeb2efb119fc1bf70f8
Clement Denis <le.mikmac@gmail.com>
2017-06-14 19:53:54 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,40 @@
+### The pilot
+If you don't have done it already, read
+[this](https://thot.psance/git/les-temoins-de-jeovascript/setup)
+to setup your workspace.
+
+
+Then `fork` this repository and `clone` your fork to start working on it.
+
+### `module.exports.add`
+export the function `add` that takes two number arguments
+and returns the result of the addition of those two.
+
+### `module.exports.sub`
+export the function `sub` that takes two number arguments
+and returns the result of the substraction of those two.
+
+### `module.exports.abs`
+export the function `abs` that takes one number argument
+and returns it's absolute value.
+
+### `module.exports.max`
+export the function `max` that returns the bigest of the two
+given number arguments.
+
+### `module.exports.min`
+export the function `min` that returns the smallest of the two
+given number arguments.
+
+### `module.exports.sign`
+export the function `sign` that takes one number argument
+and return 1 if the number is positive, -1 if then number is negative
+and 0 if the number is exactly 0
+
+### `module.exports.positive`
+export the function `positive` that takes two number arguments
+and returns if only one of them is negative.
+
+### `module.exports.multiply`
+export the function `multiply` that takes two number arguments
+and returns the result of the multiplication of those two.
---
+++ test.js
@@ -0,0 +1,87 @@
+module.exports = ({ test, describe, exports, code, $ }) => {
+  const pass = _ => _
+
+  const generateOpTests = (name, op, values, map=pass) =>
+    values.map(([ a, b ]) => test(`(${a} ${op} ${b})`)
+      .call(() => exports[name](a, b))
+      .equal(map(eval(`(${a} ${op} ${b})`))))
+
+  const testOp = (name, op, values, map) =>
+    test.fn(name, generateOpTests(name, op, values, map))
+
+  const testMath = (name, values) => test.against(name, Math[name], values)
+  const genNum = (min, max) => n => n && ((Math.random() * n * max) + (min * n))
+
+  const basePairs = [
+    [  1,  1 ],
+    [  1, -1 ],
+    [ -1,  1 ],
+    [ -1, -1 ],
+    [  0,  0 ],
+    [  0,  1 ],
+    [  1,  0 ],
+    [ -1,  0 ],
+    [  0, -1 ],
+  ]
+
+  const bigPairs = basePairs.map(n => n.map(genNum(10, 99)))
+  const smallPairs = basePairs.map(n => n.map(genNum(0, 1)))
+
+  const pairs = bigPairs.concat(smallPairs)
+  const single = pairs.map(([n]) => [n])
+
+  const flooredSingle = bigPairs.map(([n]) => [Math.floor(n)])
+  const flooredPairs = bigPairs.map(n => n.map(Math.floor))
+
+  const odds = '13579'
+  const isOdd = n => odds.indexOf(n.toString()[n.toString().length - 1]) !== -1
+
+  return [
+    describe('cheating', [
+        'require',
+        'Math',
+        'toString',
+        'String',
+        'Number',
+        'parseInt',
+        'toFixed',
+      ].map(key => test(`${key} should not be used`)
+        .value($(`#${key}`).length).equal(0))
+      .concat([
+        test('Strings should not be used')
+          .value($('String').length < 2).equal(true),
+      ])
+      .concat('~ >> ^ << * % / & |'.split(' ')
+        .map(op => test(`${op} operator`)
+          .value(code.ast.tokens.filter(t => t.value === op).length)
+          .equal(0, `You can't use the operator ${op}, try something else`)))),
+
+    testOp('add', '+', pairs),
+    testOp('sub', '-', pairs),
+
+    testMath('sign', single),
+    testMath('abs', single),
+
+    testMath('max', pairs),
+    testMath('min', pairs),
+
+    test.against('positive', (a, b) => a < 0 ? b < 0 : b > 0, pairs
+      .filter(([a, b]) => a !== 0 && b !== 0)),
+
+    testOp('multiply', '*', flooredPairs),
+    testOp('modulo', '%', flooredPairs, Math.trunc),
+    testOp('divide', '/', flooredPairs, Math.trunc),
+
+    testMath('ceil', single),
+    testMath('floor', single),
+    testMath('round', single),
+    testMath('trunc', single),
+
+    test.against('odd', isOdd, flooredSingle),
+    test.against('even', n => !isOdd(n), flooredSingle),
+
+    test.against('clamp', (n, max) => n < max ? n : max, pairs),
+    test.against('rotate', (n, max) => n % max, flooredPairs
+      .filter(([ a, b ]) => a > 0 && b > 0)),
+  ]
+}
